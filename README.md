35e2275d8c9a52f38a401250c6e903372e17da44
triring <akio@triring.net>
2026-06-08 19:48:21 +0900

    README.mdにメソッドの解説を追記

--- README.md
+++ README.md
@@ -5,16 +5,14 @@ The TM1638 is an LED driver controller with a key scan interface.
 このリポジトリでは、tinygoで作成したTM1638を制御するドライバを公開しています。  
 TM1638はキースキャンインターフェースを備えたLEDドライバコントローラです。  
 このコントローラは、「ストローブ(STB)」「クロック(CLK)」「データ(DIO)」のわずか3本の信号線で接続するだけで、複数の7セグメントLEDとスイッチ、LEDを同時に制御できます。  
-また、このコントローラ内部で自動的にダイナミック点灯（スキャン表示）とキー入力のスキャンを行うため、マイコン側のの負荷軽減してくれます。  
-ArduinoやMicroPythonで、このデバイスをコントロールするデバイスドライバはたくさんあったのですが、tinygoで書かれたものが見当たらなかったので、自作してみました。  
+また、このコントローラ内部で自動的にダイナミック点灯（スキャン表示）とキー入力のスキャンを行うため、マイコン側の負荷を大幅に軽減してくれます。  
+ArduinoやMicroPythonで、このデバイスをコントロールするドライバやライブラリはたくさんあったのですが、tinygoで書かれたものが見当たらなかったので、このパッケージを自作してみました。  
 
 
 ## ハードウェア
 
-以下のamazonで購入したTM1638の評価ボードを使用しました。2個で1000円弱でした。
-今回、検証用に使用したマイコンボードは、Raspberry Pi Picoです。
-配線は、以下のように接続しました。この接続は、I2CでもSPIでもないようなので、汎用入出力端子であればOKです。
-使用するマイコンボードの空き端子に合わせて、配線して下さい。
+以下のamazonで購入したTM1638の評価ボードを使用しました。2個で1000円弱でした。  
+検証用に使用したマイコンボードは、Raspberry Pi Picoで、これらを、以下のように接続しました。  
 
 | TM1638 | Raspberry Pi Pico |
 |:-------|:------------------|
@@ -24,9 +22,12 @@ ArduinoやMicroPythonで、このデバイスをコントロールするデバ�
 | CLK    | GPIO27            |
 | DIO    | GPIO26            |
 
+この接続は、I2CやSPIではなく、独自のプロトコルで通信しており、汎用入出力端子に割り当てればOKです。
+使用するマイコンボードの空き端子に合わせて、配線して下さい。
+
 ## ソフトウェア
 
-開発には、以下のバージョンのgo と tinygo を使用しました。
+開発には、以下のバージョンの go と tinygo を使用しました。
 
     > go version
     go version go1.26.4 windows/amd64
@@ -36,13 +37,13 @@ ArduinoやMicroPythonで、このデバイスをコントロールするデバ�
 ## 使用方法
 
 以下のコマンドで、このリポジトリの内容をローカルにコピーして下さい。
+
 ```bash
 git clone https://github.com/triring/tm1638.git
 ```
 
 コピーされたtm1638ディレクトリ内のファイル構成
 ```bash
-
 D:.
 |   .gitignore
 |   go.mod
@@ -69,9 +70,10 @@ D:.
 ```
 
 コピーしたディレクトリ内に、examplesディレクトリがあります。
+この中にテスト用コードがあります。
 
 1. ターゲットボードとtm1638評価ボードを3本の信号線、電源、GND線で接続して下さい。
-2. PCとターゲットボードを接続して下さい。
+2. PCとターゲットボードをUSBケーブルで接続して下さい。
 3. 実行したいコードのあるディレクトリ内に移動して下さい。
 4. 以下のコマンドで、コンパイル&実行ファイルの転送を行って下さい。  
 (-targetオプションは、使用するマイコンボードに合わせて修正して下さい。)
@@ -82,7 +84,7 @@ tinygo flash -target=pico -size=short -monitor .
 
 ## 解説
 
-このドライバを使うと、以下のようなコードでTM1638を制御し、簡単にLチカができます。
+このドライバを使うと、以下のようなコードでTM1638を制御し、簡単に8連Lチカができます。
 
 1. "github.com/triring/tm1638"をインポートする。
 2. tm1638の入出力ピンを設定する。
@@ -97,13 +99,14 @@ import (
 )
 
 func main() {
-	// ピンの初期化（ピン番号はPicoの実際の配線に合わせて変更してください）
+	// 入出力ピンの設定
+    // ピン番号は使用するマイコンボードとtm1638の配線に合わせて変更してください）
 	stbPin := machine.GP28
 	clkPin := machine.GP27
 	dioPin := machine.GP26
 
 	TM1638 := tm1638.New(stbPin, clkPin, dioPin)
-	TM1638.Setup()
+	TM1638.Setup()  // tm1638の初期化
 	for {
 			TM1638.SetLEDs(0x55)
 			time.Sleep(500 * time.Millisecond)
@@ -114,16 +117,118 @@ func main() {
 ```
 ## メソッドの使い方
 
+準備中
+### 初期化等の基本制御
+
+* func New(stbPin machine.Pin, clkPin machine.Pin, dioPin machine.Pin) Device
+    * tm1638 ドライバを設定します。
+        - stbPin:strobePinの設定(Chip select)
+        - clkPin:clockPinの設定(Clock input)
+        - dioPin:dataPinの設定(DataI/O)
+
+* func (d *Device) Setup() 
+    * tm1638の初期化を行います。Newの後に必ず実行して下さい。
+
+* 表示の明るさを設定します。設定範囲は、0-7までです。
+    * func (d *Device) SetBrightness(val byte)
+
+* func (d *Device) reset()
+    * tm1638 をリセットします。
+
+* func (d *Device) sendCMD(cmd byte)
+    * tm1638 に1byteの制御命令を送ります。
+
+* func (d *Device) sendDATA(data byte, position int)
+    * tm1638に 1文字のデータを送ります.
+
+* func (d *Device) sendDATAs(data [8]byte)
+    * tm1638に 8byteのデータを送ります。
+
+* func (d *Device) shift_out(val byte)
+    * データ送信 (shift_out相当)
+    * tm1638に1byteのデータを送信する。
+        - val: 送信データ
+
+* func (d *Device) shift_in() (val byte)
+    * データ受信 (shift_in相当)
+    * tm1638から1byteのデータを受信する。
+        - val: 受信データ
 
 ### 7セグLEDの制御
 
+* func (d *Device) Disp7SEG(segments byte, position int)
+    * 指定した桁にセグメントデータを送信します。
+        - segments: 1つの7セグメントLEDの点灯パターン
+        - position: 点灯する7セグメントLEDの位置
+
+* func (d *Device) Disp7SEGs(segments [8]byte)
+    * 8桁分のセグメントデータを一括送信する
+        - segments: 8バイトの配列 (各バイトが1つの7セグメントLEDに対応)
+
+* func (d *Device) ScrollingText(scrollTextData []byte, interval time.Duration)
+    * 文字列のスクロール表示
+        - scrollTextData: 表示する文字列データ
+        - interval: 文字の表示間隔(単位は、ms)
+
 ### 7セグ表示データの生成
+
+* func (d *Device) Get7Seg(r rune) (byte, error)
+    * 一文字のデータを7セグメント用のデータに変換します。
+        - r: 一文字のデータ,rune型です。byte型ではないことに注意して下さい。
+
+* func (d *Device) StrTo7Seg(text string) [8]byte
+    * 文字列を7セグメント用の8byteデータに変換します。
+
+* func (d *Device) IntTo7Seg(n int) [8]byte
+    * 整数を7セグメント用の8byteデータに変換します。
+    * 負の数や桁溢れ等には対応していません。
 
 ### 8連LEDの制御
 
+* func (d *Device) SetLED(value byte, position byte)
+    * 指定する1つのLEDの値を設定します。
+        - value: 設定値
+        - position: LEDの番号
+
+* func (d *Device) SetLEDs(value byte)
+    * LEDの点灯パターンを設定します。
+        - value: 1byteのデータで構成される点灯パターン
+
+* func (d *Device) ClearLEDs()
+    * 全LEDを消灯する。
 
 ### 8連キーからの入力
 
+* func (d *Device) ScanKeys() byte
+    * 8個のキーのうち、押されているキーの状態を1byteのデータとして返します。LSBはSW1です。
+
+## サンプルコードについて
+
+[Blink8LED/main.go](Blink8LED/main.go)  
+
+    8連LEDを点滅させます。
+
+[DispNum/main.go](DispNum/main.go)  
+
+    7セグに数字の表示します。
+
+[HelloWorld/main.go](HelloWorld/main.go)  
+
+    7セグに文字列"Hello"を疑似アルファベットで表示します。
+
+[Keytest/main.go](Keytest/main.go)  
+
+    8個のキーの入力状態を読取ります。押されているキーに対応するLEDを消灯します。
+
+[LEDtest/main.go](LEDtest/main.go)  
+
+    8連LEDをいろいろなパターンで点滅させます。
+
+[ScrollingText/main.go](ScrollingText/main.go)  
+
+    7セグに文字列を疑似アルファベットでスクロール表示します。
+
 ## このパッケージのドキュメント
 
+以下のリンクをご覧ください。
 [package tm1638のドキュメント](https://pkg.go.dev/github.com/triring/tm1638)
--- tm1638.go
+++ tm1638.go
@@ -47,7 +47,7 @@ func New(stbPin machine.Pin, clkPin machine.Pin, dioPin machine.Pin) Device {
 
 // Setting up the tm1638.
 // 
-// tm1638の初期化を行います。Newの後にか鳴らす実行して下さい。
+// tm1638の初期化を行います。Newの後に必ず実行して下さい。
 func (d *Device) Setup() {
 	d.sendCMD(cmdActivate) // activate	0x8f
 	d.reset()
@@ -130,6 +130,108 @@ func (d *Device) sendDATAs(data [8]byte) {
 	time.Sleep(tmDelay)
 }
 
+// Sends 1 byte of data to tm1638.
+//
+// データ送信 (shift_out相当)
+// tm1638に1byteのデータを送信する。
+// val: 送信データ
+func (d *Device) shift_out(val byte) {
+	d.dioPin.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	for i := 0; i < 8; i++ {
+		// LSB First
+		bit := (val >> i) & 1
+		d.dioPin.Set(bit == 1)
+		time.Sleep(tmDelay)
+
+		d.clkPin.High()
+		time.Sleep(tmDelay)
+
+		d.clkPin.Low()
+		time.Sleep(tmDelay)
+	}
+}
+
+// Receives 1 byte of data from tm1638.
+//
+// データ受信 (shift_in相当)
+// tm1638から1byteのデータを受信する。
+// val: 受信データ
+func (d *Device) shift_in() (val byte) {
+	var value byte
+	d.dioPin.Configure(machine.PinConfig{Mode: machine.PinInput})
+	time.Sleep(tmDelay)
+	for i := 0; i < 8; i++ {
+		d.clkPin.Low()
+		time.Sleep(tmDelay)
+		d.clkPin.High()
+		// クロック立ち上がりで値を読み取る
+		if d.dioPin.Get() {
+			value |= (1 << i)
+		}
+		time.Sleep(tmDelay)
+	}
+	d.clkPin.Low()
+	return value
+}
+
+// Sends segment data to a specified digit.
+//
+// 指定した桁にセグメントデータを送信します。
+// segments: 1つの7セグメントLEDの点灯パターン
+// position: 点灯する7セグメントLEDの位置
+func (d *Device) Disp7SEG(segments byte, position int) {
+	// 1. データ設定コマンド送信 (書き込みモード、アドレス自動加算)
+	d.sendCMD(cmdDataSettings)
+	// 2. 表示アドレスの指定とデータ送信
+	d.sendDATA(segments, position)
+	// 3. 表示の有効化と輝度設定
+	d.sendCMD(cmdDisplayBright | cmdDisplayOnOff | d.brightness) // 0x88(ON) | 0x07(最大輝度)
+	// d.sendCMD(cmdDisplayBright | cmdDisplayOnOff | 0x07) // 0x88(ON) | 0x07(最大輝度)
+	// cmdDisplayBright = 0x80 // ディスプレイ制御 (輝度設定)
+	// cmdDisplayOnOff  = 0x08 // ディスプレイ制御 (表示ON_OFF)
+}
+
+
+// Sends 8-digit segment data in a single batch.
+//
+// 8桁分のセグメントデータを一括送信する
+// segments: 8バイトの配列 (各バイトが1つの7セグメントLEDに対応)
+func (d *Device) Disp7SEGs(segments [8]byte) {
+	// 1. データ設定コマンド送信 (書き込みモード、アドレス自動加算)
+	d.sendCMD(cmdDataSettings)
+	// 2. 表示アドレスの指定とデータ送信
+	d.sendDATAs(segments)
+	// 3. 表示の有効化と輝度設定
+	d.sendCMD(cmdDisplayBright | cmdDisplayOnOff | d.brightness) // 0x88(ON) | 0x07(最大輝度)
+	// d.sendCMD(cmdDisplayBright | cmdDisplayOnOff | 0x07) // 0x88(ON) | 0x07(最大輝度)
+	// cmdDisplayBright = 0x80 // ディスプレイ制御 (輝度設定)
+	// cmdDisplayOnOff  = 0x08 // ディスプレイ制御 (表示ON_OFF)
+}
+
+// Scroll text display
+//
+// 文字列のスクロール表示
+// scrollTextData: 表示する文字列データ
+// interval: 文字の表示間隔(単位は、ms)
+func (d *Device) ScrollingText(scrollTextData []byte, interval time.Duration) {
+	scrollLength := len(scrollTextData)
+	var index int = 0
+	var dispData [8]byte
+	for {
+		for i := 0; i < 8; i++ {
+			dispData[i] = scrollTextData[index+i]
+		//	fmt.Printf("%c ", dispData[i])
+		}
+	//	fmt.Printf("\n")
+		d.Disp7SEGs(dispData)
+		time.Sleep(interval * time.Millisecond)
+		index++
+		if index > (scrollLength - 8) {
+			break
+		}
+	}
+}
+
 // ScanKeys Return a byte representing which keys are pressed. LSB is SW1
 // 
 // 8個のキーのうち、押されているキーの状態を1byteのデータとして返します。LSBはSW1です。
@@ -189,105 +291,3 @@ func (d *Device) ClearLEDs() {
 	}
 }
 
-// Sends 8-digit segment data in a single batch.
-//
-// 8桁分のセグメントデータを一括送信する
-// segments: 8バイトの配列 (各バイトが1つの7セグメントLEDに対応)
-func (d *Device) Disp7SEGs(segments [8]byte) {
-	// 1. データ設定コマンド送信 (書き込みモード、アドレス自動加算)
-	d.sendCMD(cmdDataSettings)
-	// 2. 表示アドレスの指定とデータ送信
-	d.sendDATAs(segments)
-	// 3. 表示の有効化と輝度設定
-	d.sendCMD(cmdDisplayBright | cmdDisplayOnOff | d.brightness) // 0x88(ON) | 0x07(最大輝度)
-	// d.sendCMD(cmdDisplayBright | cmdDisplayOnOff | 0x07) // 0x88(ON) | 0x07(最大輝度)
-	// cmdDisplayBright = 0x80 // ディスプレイ制御 (輝度設定)
-	// cmdDisplayOnOff  = 0x08 // ディスプレイ制御 (表示ON_OFF)
-}
-
-// Sends segment data to a specified digit.
-//
-// 指定した桁にセグメントデータを送信します。
-// segments: 1つの7セグメントLEDの点灯パターン
-// position: 点灯する7セグメントLEDの位置
-func (d *Device) Disp7SEG(segments byte, position int) {
-	// 1. データ設定コマンド送信 (書き込みモード、アドレス自動加算)
-	d.sendCMD(cmdDataSettings)
-	// 2. 表示アドレスの指定とデータ送信
-	d.sendDATA(segments, position)
-	// 3. 表示の有効化と輝度設定
-	d.sendCMD(cmdDisplayBright | cmdDisplayOnOff | d.brightness) // 0x88(ON) | 0x07(最大輝度)
-	// d.sendCMD(cmdDisplayBright | cmdDisplayOnOff | 0x07) // 0x88(ON) | 0x07(最大輝度)
-	// cmdDisplayBright = 0x80 // ディスプレイ制御 (輝度設定)
-	// cmdDisplayOnOff  = 0x08 // ディスプレイ制御 (表示ON_OFF)
-}
-
-// Scroll text display
-//
-// 文字列のスクロール表示
-// scrollTextData: 表示する文字列データ
-// interval: 文字の表示間隔(単位は、ms)
-func (d *Device) ScrollingText(scrollTextData []byte, interval time.Duration) {
-	scrollLength := len(scrollTextData)
-	var index int = 0
-	var dispData [8]byte
-	for {
-		for i := 0; i < 8; i++ {
-			dispData[i] = scrollTextData[index+i]
-		//	fmt.Printf("%c ", dispData[i])
-		}
-	//	fmt.Printf("\n")
-		d.Disp7SEGs(dispData)
-		time.Sleep(interval * time.Millisecond)
-		index++
-		if index > (scrollLength - 8) {
-			break
-		}
-	}
-}
-
-// Sends 1 byte of data to tm1638.
-//
-// データ送信 (shift_out相当)
-// tm1638に1byteのデータを送信する。
-// val: 送信データ
-func (d *Device) shift_out(val byte) {
-	d.dioPin.Configure(machine.PinConfig{Mode: machine.PinOutput})
-	for i := 0; i < 8; i++ {
-		// LSB First
-		bit := (val >> i) & 1
-		d.dioPin.Set(bit == 1)
-		time.Sleep(tmDelay)
-
-		d.clkPin.High()
-		time.Sleep(tmDelay)
-
-		d.clkPin.Low()
-		time.Sleep(tmDelay)
-	}
-}
-
-// Receives 1 byte of data from tm1638.
-//
-// データ受信 (shift_in相当)
-// tm1638から1byteのデータを受信する。
-// val: 受信データ
-func (d *Device) shift_in() (val byte) {
-	var value byte
-	d.dioPin.Configure(machine.PinConfig{Mode: machine.PinInput})
-	time.Sleep(tmDelay)
-
-	for i := 0; i < 8; i++ {
-		d.clkPin.Low()
-		time.Sleep(tmDelay)
-
-		d.clkPin.High()
-		// クロック立ち上がりで値を読み取る
-		if d.dioPin.Get() {
-			value |= (1 << i)
-		}
-		time.Sleep(tmDelay)
-	}
-	d.clkPin.Low()
-	return value
-}
